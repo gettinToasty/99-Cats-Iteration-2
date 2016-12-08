0772a2fd8a5c64a16fea6754d2ea1d2bd4cb8eed
gettinToasty <sean.r.beyer@gmail.com>
2016-12-07 21:50:53 -0800

    Fix multiple session implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,25 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def log_in(user)
-    user.device = request.env["HTTP_USER_AGENT"]
-    user.ensure_session_token
-    session[:session_token] = user.sessions.find_by(device: user.device).session_token
+    token = SecureRandom.urlsafe_base64(128)
+    device = request.env["HTTP_USER_AGENT"]
+    @session = Session.new(user_id: user.id,
+                             session_token: token,
+                             device: device
+                            )
+    @session.save
+    session[:session_token] = @session.session_token
   end
 
   def log_out
-    current_user.reset_session_token! if current_user
+    device = request.env["HTTP_USER_AGENT"]
+    @session = Session.find_by(device: device)
+    @session.destroy if current_user
   end
 
   def current_user
-    @current_user = Session.find_by(session_token: session[:session_token]).user if Session.find_by(session_token: session[:session_token])
+    current_session = Session.find_by(session_token: session[:session_token])
+    @current_user = current_session.user if current_session
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
 
   attr_reader :password, :device
 
-  after_initialize :ensure_session_token
-
   has_many :cats
 
   has_many :rental_requests,
@@ -28,23 +26,6 @@ class User < ActiveRecord::Base
 
   def is_password?(pw)
     BCrypt::Password.new(password_digest).is_password?(pw)
-  end
-
-  def device=(device)
-    @device = device
-  end
-
-  def generate_session_token
-    token = SecureRandom.urlsafe_base64(128)
-    Session.create(session_token: token, user_id: self.id, device: @device)
-  end
-
-  def reset_session_token!
-    Session.find_by(device: @device).destroy
-  end
-
-  def ensure_session_token
-    generate_session_token unless Session.exists?(device: @device)
   end
 
 end
